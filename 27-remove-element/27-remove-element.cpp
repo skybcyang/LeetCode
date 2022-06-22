8663d61f18a6c8a59986b7a0d19c135ad1e79396
SkybcYang <skybcyang@qq.com>
2022-06-23 01:10:30 +0800

    Time: 3 ms (66.24%), Space: 8.8 MB (72.55%) - LeetHub

---
+++ 27-remove-element/27-remove-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if(nums.size()==0)return 0;
+        int i=0, j=nums.size()-1;
+        while(i != j){
+            if(nums[i] == val){
+                nums[i] = nums[j];
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        if(nums[i]==val){
+            i = i-1;
+        }
+        return i+1;
+    }
+};
